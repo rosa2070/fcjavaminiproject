c8a7493a20d57dfd1a9d85cf1d74af6064f8c2ab
rosa2 <rosa2070@naver.com>
2024-01-18 18:24:36 +0900

    Initial commit

---
+++ src/main/java/kr/excel/example/ExcelExample.java
@@ -0,0 +1,26 @@
+package kr.excel.example;
+
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ExcelExample {
+    public static void main(String[] args) {
+        try {
+            FileInputStream file = new FileInputStream(new File("example.xlsx"));
+            Workbook workbook= WorkbookFactory.create(file);
+            Sheet sheet=workbook.getSheetAt(0);
+            for (Row row : sheet) {
+                for (Cell cell : row) {
+                    System.out.print(cell.toString() + "\t");
+                }
+                System.out.println();
+            }
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
